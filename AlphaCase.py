0d2a9bdfe80f4057cddb361bc572fd5ec27c7b6d
shivamsam050 <31090066+shivamsam050@users.noreply.github.com>
2017-08-23 22:47:42 -0700

    Add files via upload

---
+++ AlphaCase.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 11:05:49 2017
+
+@author: sharda
+"""
+
+x=input("inter the character")
+if((x>=a) and (x<=z)):
+    printf(x.' is a loweer case')
